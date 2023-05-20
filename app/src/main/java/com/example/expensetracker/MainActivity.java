354ab145093c5ead2002bdd8d23ad51ec864c29c
PetarVukcevic <petar.vukcevic2001@gmail.com>
2023-05-20 15:47:33 +0200

    insert statement to Expenses and Incomes tables, sum of "amount" column

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.expensetracker;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -35,13 +37,64 @@ public class MainActivity extends Activity {
         // Create an instance of your DatabaseHelper
         databaseHelper = new DatabaseHelper(this);
 
+
+//        insert("incomes", "Salary", "This is my salary", 200.5F);
+        float sumOfExpenses = getSumOfAmount("expenses");
+        float sumOfIncomes = getSumOfAmount("incomes");
+        mHeadingTextView.setText(Float.toString(sumOfIncomes - sumOfExpenses));
+    }
+
+    public void insert(String table, String title, String description, Float amount) {
+        // Create an instance of your DatabaseHelper
+        databaseHelper = new DatabaseHelper(this);
+
         // Get a reference to the database
         SQLiteDatabase database = databaseHelper.getWritableDatabase();
 
-        // Perform database operations as needed
+        // Create a ContentValues object to hold the values you want to insert
+        ContentValues values = new ContentValues();
+        values.put("title", title);
+        values.put("description", description);
+        values.put("amount", amount);
+        values.put("created_at", System.currentTimeMillis());
 
-        // Remember to close the database when you're done
+        // Insert the values into the table
+        long rowId = database.insert(table, null, values);
+
+        if (rowId != -1) {
+            // Insertion successful
+            // rowId contains the ID of the newly inserted row
+            System.out.println("Uspjesno opet");
+        } else {
+            // Insertion failed
+            System.out.println("Ne valja kurca");
+
+        }
         database.close();
+    }
+
+    public float getSumOfAmount(String tableName) {
+            // Assuming you have a readable database object
+            SQLiteDatabase database = databaseHelper.getReadableDatabase();
+
+            // Define the column to sum
+            String columnToSum = "amount";
+
+            // Execute the query to calculate the sum
+            String query = "SELECT SUM(" + columnToSum + ") FROM " + tableName;
+            Cursor cursor = database.rawQuery(query, null);
+
+            // Retrieve the sum value from the cursor
+            float totalAmount = 0.0f;
+            if (cursor.moveToFirst()) {
+                totalAmount = cursor.getFloat(0);
+            }
+
+            // Close the cursor and database when you're done
+            cursor.close();
+            database.close();
+
+            return totalAmount;
     }
 
 }
